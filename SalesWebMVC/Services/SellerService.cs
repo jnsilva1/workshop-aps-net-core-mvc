0e9b8c177a28543c2e2185466e3774232eaed73f
José Silva <josenilo.cp@gmail.com>
2020-02-02 14:54:05 -0300

    Exception handling for delete (referencial integrity)

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -30,33 +30,36 @@ namespace SalesWebMVC.Services
 
         public async Task<Seller> FindByIdAsync(int id)
         {
-            return await _context.Seller.Include(obj=> obj.Departament).FirstOrDefaultAsync(obj => obj.Id == id);
+            return await _context.Seller.Include(obj => obj.Departament).FirstOrDefaultAsync(obj => obj.Id == id);
         }
 
         public async Task RemoveAsync(int id)
         {
-            await RemoveAsync( await _context.Seller.FindAsync(id));
+            await RemoveAsync(await _context.Seller.FindAsync(id));
         }
 
         public async Task RemoveAsync(Seller obj)
         {
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e) { throw new IntegrityException(e.Message); }
+
         }
 
         public async Task UpdateAsync(Seller obj)
         {
             bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
-            if (!hasAny)
-            {
-                throw new NotFoundException("Id Not Found");
-            }
+            if (!hasAny) { throw new NotFoundException("Id Not Found"); }
             try
             {
                 _context.Update(obj);
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException e ) {
+            catch (DbUpdateConcurrencyException e)
+            {
                 throw new DbConcurrencyException(e.Message);
             }
         }
